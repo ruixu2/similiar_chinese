db70c3f87ec82b7f9b4b3d754958f0ee04b275e2
ruixu <rare0502@foxmail.com>
2024-05-04 19:26:09 +0800

    set github action

--- code/03_get_the_most_similair.py
+++ code/03_get_the_most_similair.py
@@ -2,9 +2,11 @@ import csv
 
 char = input("输入汉字：")
 if len(char) != 1:
+    # if char is not in range(0x4E00, 0x9FA5 + 1):
+    #
     char = "我"
 temp = ord(char)
-print(char, temp)
+# print(char, temp)
 
 csv_path = f"../similiar/{temp}_{char}.csv"
 f = open(csv_path, encoding='utf-8')
@@ -12,4 +14,4 @@ df = csv.reader(f)
 for idx, row in enumerate(df):
     if idx < 2:
         continue
-    print(row)
+    print(row[1])
